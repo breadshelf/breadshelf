9dc86657f1075fba10025b3535001e208a8d024a
Noah Celuch <noah.celuch@gmail.com>
2025-11-16 22:56:35 -0500

    feat: initial sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 
 class UsersController < ApplicationController
-  def index; end
+  before_action :set_user, only: [:show]
+
+  def new
+    @user = User.new
+  end
+
+  def show
+  end
+
+  def create
+    @user = User.new(sign_up_params)
+
+    if @user.save
+      respond_to do |format|
+        format.html { redirect_to @user }
+      end
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def sign_up_params
+    params.require(:user).permit(:email)
+  end
 end
