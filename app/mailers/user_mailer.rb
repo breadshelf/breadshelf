82b15b037dca2cc2f82a8ed1238913539bc0e6c6
Noah Celuch <noah.celuch@gmail.com>
2026-01-08 22:10:36 -0500

    fix: email fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,8 @@ class UserMailer < ApplicationMailer
     @user = params[:user]
     @base_url = "#{ENV['DOMAIN']}"
     @about_url = "#{@base_url}/about"
-    attachments.inline['slice.svg'] = File.read(Rails.root.join('app/assets/images/slice.svg'))
-    attachments.inline['crumbs.svg'] = File.read(Rails.root.join('app/assets/images/crumbs.svg'))
+    attachments.inline['slice.png'] = File.read(Rails.root.join('app/assets/images/slice.png'))
+    attachments.inline['crumbs.png'] = File.read(Rails.root.join('app/assets/images/crumbs.png'))
     mail(to: @user.email, subject: 'Welcome to breadshelf')
   end
 end
