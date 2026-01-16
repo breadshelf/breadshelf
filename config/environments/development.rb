27762b388f908023900c043e1d31bba1257dc443
Noah Celuch <noah.celuch@gmail.com>
2026-01-15 22:05:24 -0500

    chore: development action mailer SES config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,13 +32,22 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
-  # Make template changes take effect immediately.
-  config.action_mailer.perform_caching = false
+  # # Make template changes take effect immediately.
+  # config.action_mailer.perform_caching = false
 
-  # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # # Set localhost to be used by links generated in mailer templates.
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.credentials.dig(:smtp, :user_name),
+    password: Rails.application.credentials.dig(:smtp, :password),
+    address: 'email-smtp.us-east-1.amazonaws.com',
+    port: 587,
+    authentication: :plain
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
