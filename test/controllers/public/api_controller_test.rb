52c40a3fc1843e76ba331fdcdeaf093bbaf051e3
Noah Celuch <noah.celuch@gmail.com>
2026-01-15 22:42:17 -0500

    test: update deliverability event test

--- test/controllers/public/api_controller_test.rb
+++ test/controllers/public/api_controller_test.rb
@@ -4,6 +4,7 @@ module Public
   class ApiControllerTest < ActionDispatch::IntegrationTest
     test 'deliverability event logs the event and responds with ok' do
       Public::Emails::Suppressor.expects(:call).once.returns(true)
+      Aws::SNS::MessageVerifier.any_instance.stubs(:authentic?).returns(true)
       post '/api/deliverability_event', params: { event: 'bounce', email: 'help@email.com' }, as: :json
       assert_response :ok
     end
