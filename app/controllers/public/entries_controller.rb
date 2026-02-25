4618caf68635db98fb697a51b7963d9d8b4c2c1e
Noah Celuch <noah.celuch@gmail.com>
2026-02-24 23:23:04 -0500

    refactor: use turbo frame for entry steps

--- app/controllers/public/entries_controller.rb
+++ app/controllers/public/entries_controller.rb
@@ -10,12 +10,7 @@ module Public
 
       if current_user
         entry = Public::Entries::Create.call(user: current_user, book_title: book_title, author_name: author_name)
-        respond_to do |format|
-          format.turbo_stream do
-            render turbo_stream: turbo_stream.replace('entry-form', partial: 'success')
-          end
-          format.html { redirect_to entry_path(entry), notice: { message: 'Entry created successfully', status: 'success' } }
-        end
+        redirect_to entry_path(entry), notice: { message: 'Entry created successfully', status: 'success' }
       else
         redirect_to new_entry_path, alert: { message: 'Please sign in to create an entry', status: 'error' }
       end
