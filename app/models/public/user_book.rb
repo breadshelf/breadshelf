c1bea4ea34dfd3ef4642507d1b3d8963311f08fb
Noah Celuch <noah.celuch@gmail.com>
2026-03-11 23:14:27 -0400

    feat: add user book table

---
+++ app/models/public/user_book.rb
@@ -0,0 +1,9 @@
+module Public
+  class UserBook < ApplicationRecord
+    belongs_to :user, class_name: 'Public::User'
+    belongs_to :book, class_name: 'Public::Book'
+
+    validates :user, presence: true
+    validates :book, presence: true
+  end
+end
